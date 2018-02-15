d7db05ebef4d0b1bab8913514752a5817ab97a20
Jean Rodrigo Dalbon Cunha <cin_jcunha@uolinc.com>
2018-02-15 10:30:10 -0200

    Initial commit project Tweet Feeling

---
+++ app/src/main/java/br/eng/jerodac/tweetfeeling/MainActivity.java
@@ -0,0 +1,9 @@
+package br.eng.jerodac.tweetfeeling;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jean Rodrigo Dalbon Cunha on 15/02/2018.
+ */
+public class MainActivity extends AppCompatActivity {
+}
