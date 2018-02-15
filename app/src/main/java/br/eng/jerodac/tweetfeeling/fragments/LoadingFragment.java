3886d5d7056d5dd18628ba3c68592240d51323cb
Jean Rodrigo Dalbon Cunha <cin_jcunha@uolinc.com>
2018-02-15 16:32:08 -0200

    Create fragment loader

---
+++ app/src/main/java/br/eng/jerodac/tweetfeeling/fragments/LoadingFragment.java
@@ -0,0 +1,29 @@
+package br.eng.jerodac.tweetfeeling.fragments;
+
+import android.view.View;
+
+/**
+ * Created by Jean Rodrigo Dalbon Cunha on 15/02/2018.
+ */
+public class LoadingFragment extends BaseFragment {
+
+    @Override
+    public String getTagName() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return 0;
+    }
+
+    @Override
+    protected void initComponents(View rootView) {
+
+    }
+
+    @Override
+    protected void settings(View rootView) {
+
+    }
+}
