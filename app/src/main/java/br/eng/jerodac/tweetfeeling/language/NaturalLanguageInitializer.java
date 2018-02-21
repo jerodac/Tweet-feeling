1b93d8e18e5f72c72843a4804f855a3f1b98c6f9
Eng. Jean Rodrigo D. Cunha <jerodac@gmail.com>
2018-02-21 00:41:19 -0300

    Adjust code

--- app/src/main/java/br/eng/jerodac/tweetfeeling/language/NaturalLanguageInitializer.java
+++ app/src/main/java/br/eng/jerodac/tweetfeeling/language/NaturalLanguageInitializer.java
@@ -5,12 +5,15 @@ import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.services.language.v1beta2.CloudNaturalLanguage;
 import com.google.api.services.language.v1beta2.CloudNaturalLanguageRequestInitializer;
 
+import br.eng.jerodac.tweetfeeling.BuildConfig;
+
 /**
  * Created by Jean Rodrigo Dalbon Cunha on 16/02/2018.
  */
 public class NaturalLanguageInitializer {
 
-    private static final String CLOUD_API_KEY = "AIzaSyBo6TfvBXn2aO6sZftBW8mZvKHmnQYHST8";
+    private static final String CLOUD_API_KEY = BuildConfig.CLOUD_API_KEY;
+
     private static CloudNaturalLanguage naturalLanguageService;
 
     public static void init() {
--- app/src/main/java/br/eng/jerodac/tweetfeeling/utils/AppUtil.java
+++ app/src/main/java/br/eng/jerodac/tweetfeeling/utils/AppUtil.java
@@ -24,16 +24,7 @@ public class AppUtil {
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static void setStatusBarColor(AppCompatActivity activity, int resourceColor) {
-
         Window window = activity.getWindow();
-
-//        // clear FLAG_TRANSLUCENT_STATUS flag:
-//        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//
-//        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-//        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-        // finally change the color
         window.setStatusBarColor(ContextCompat.getColor(activity, resourceColor));
     }
 
